0b65c5f5e4bc5b3836ea384aa466f5b2437a4d4f
Stefan Müller zzeroo <co@zzeroo.com>
2020-09-06 23:19:49 +0200

    Rename function and add documentation and test

--- src/registers/mod.rs
+++ src/registers/mod.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
 use serde::{de::DeserializeOwned, Deserialize};
+use std::fs::File;
 
 /// Read only register
 #[derive(Debug, Default, Deserialize)]
@@ -28,19 +28,40 @@ pub struct Rwreg {
     protected: String,
 }
 
-/// Generates a Vec of Rreg from the given csv file
-pub fn from_csv<'a, T>(file_path: &str) -> Result<Vec<T>, Box<dyn std::error::Error>> 
-    where T: DeserializeOwned,
+/// Generic function to generates a Vec of `Deserializable` types (Foo in that case)
+///
+/// # Examples
+/// This example assumes a test file under /tmp!
+/// Create one e.g. `echo "field\n1337">/tmp/test.csv`
+///
+/// ```rust,no_run
+/// use rgms_konfig::registers::vec_from_csv;
+/// use serde::{de::DeserializeOwned, Deserialize};
+///
+/// #[derive(Deserialize)]
+/// struct Foo {
+///     field: usize,
+/// }
+///
+/// fn main() {
+///     let file_path = "/tmp/test.csv";
+///     let res: Result<Vec<Foo>, Box<dyn std::error::Error>> = vec_from_csv(&file_path);
+///     assert!(res.is_ok());
+///     assert!(res.unwrap().len() == 1)
+/// }
+pub fn vec_from_csv<'a, T>(file_path: &str) -> Result<Vec<T>, Box<dyn std::error::Error>>
+where
+    T: DeserializeOwned,
 {
     let file = File::open(file_path).unwrap();
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
         .from_reader(file);
     let mut res: Vec<T> = vec![];
-        for result in rdr.deserialize() {
+    for result in rdr.deserialize() {
         let record: T = result?;
         res.push(record);
-    };
+    }
     Ok(res)
 }
 
@@ -49,17 +70,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_from_csv_rreg() {
+    fn test_vec_from_csv_rreg() {
         let file_path = "src/sensors/sensor_mb_ne4_legacy-rregs.csv";
-        let res: Result<Vec<Rreg>, Box<dyn std::error::Error>> = from_csv(&file_path);
+        let res: Result<Vec<Rreg>, Box<dyn std::error::Error>> = vec_from_csv(&file_path);
         assert!(res.is_ok());
         assert!(res.unwrap().len() == 16)
     }
 
     #[test]
-    fn test_from_csv_rwreg() {
+    fn test_vec_from_csv_rwreg() {
         let file_path = "src/sensors/sensor_mb_ne4_legacy-rwregs.csv";
-        let res: Result<Vec<Rwreg>, Box<dyn std::error::Error>> = from_csv(&file_path);
+        let res: Result<Vec<Rwreg>, Box<dyn std::error::Error>> = vec_from_csv(&file_path);
         assert!(res.is_ok());
         assert!(res.unwrap().len() == 44)
     }
